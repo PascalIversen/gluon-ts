fixed mixture gradient if both distribution have 0 density, fixed beta density

src/gluonts/mx/distribution/beta.py
@@ -23,11 +23,12 @@ from gluonts.core.component import validated
 
 # First-party imports
 from gluonts.model.common import Tensor
-from gluonts.support.util import erf, erfinv
 
 # Relative imports
 from .distribution import Distribution, _sample_multiple, getF, softplus
 from .distribution_output import DistributionOutput
+from .mixture import MixtureDistributionOutput
+from .deterministic import DeterministicOutput
 
 
 class Beta(Distribution):
@@ -70,13 +71,16 @@ class Beta(Distribution):
         F = self.F
         alpha, beta = self.alpha, self.beta
 
-        return (
+        x = F.where(F.logical_and(x > 0, x < 1), x, x.zeros_like() + 0.5)
+        pdf = (
             (alpha - 1) * F.log(x)
             + (beta - 1) * F.log(1 - x)
             - F.gammaln(alpha)
             - F.gammaln(beta)
             + F.gammaln(alpha + beta)
         )
+        # pdf definition does not extend for x not in (0,1)
+        return F.where(F.logical_and(x > 0, x < 1), pdf, -1 / pdf.zeros_like())
 
     @property
     def mean(self) -> Tensor:
@@ -156,3 +160,20 @@ class BetaOutput(DistributionOutput):
     @property
     def value_in_support(self) -> float:
         return 0.5
+
+
+class ZeroInflatedBetaOutput(MixtureDistributionOutput):
+    def __init__(self):
+        super().__init__([BetaOutput(), DeterministicOutput(0)])
+
+
+class OneInflatedBetaOutput(MixtureDistributionOutput):
+    def __init__(self):
+        super().__init__([BetaOutput(), DeterministicOutput(1)])
+
+
+class ZeroOneInflatedBetaOutput(MixtureDistributionOutput):
+    def __init__(self):
+        super().__init__(
+            [BetaOutput(), DeterministicOutput(0), DeterministicOutput(1)]
+        )

src/gluonts/mx/distribution/deterministic.py
@@ -60,7 +60,7 @@ class Deterministic(Distribution):
     def event_dim(self) -> int:
         return 0
 
-    def log_prob(self, x: Tensor) -> Tensor:
+    def log_prob_pmf(self, x: Tensor) -> Tensor:
         F = self.F
         value = self.value
         is_both_nan = F.broadcast_logical_and(x != x, value != value)
@@ -68,6 +68,19 @@ class Deterministic(Distribution):
             (x == value), is_both_nan
         )
         return F.log(is_equal_or_both_nan)
+
+    def log_prob(self, x: Tensor) -> Tensor:
+        F = self.F
+        value = self.value
+        is_both_nan = F.broadcast_logical_and(x != x, value != value)
+        is_equal_or_both_nan = F.broadcast_logical_or(
+            (x == value), is_both_nan
+        )
+        return F.where(
+            is_equal_or_both_nan,
+            x.zeros_like() + 100000,
+            x.zeros_like() - 100000,
+        )
 
     @property
     def mean(self) -> Tensor: